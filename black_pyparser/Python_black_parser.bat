fb12cb73057727dc3c11f7100855e92cea0e8df0
MaxxImmersive <luis.lopez@maxx-immersive.com>
2023-04-28 23:29:13 -0500

    ... added drag drop .bat file
    found more trip ups testing on that ugly evel.py_ file

---
+++ black_pyparser/Python_black_parser.bat
@@ -0,0 +1,7 @@
+
+black %1
+
+perl  TabsPythonParser.pl %1 > %1_.vfc
+
+timeout 5
+
--- black_pyparser/plrun.bat
+++ black_pyparser/plrun.bat
@@ -1,7 +1,8 @@
 cls
 black test.py_
+
 perl  %1.pl test.py_  
-perl  %1.pl test.py_   >  test.py.vfc
+
 pause
 timeout 5
 
